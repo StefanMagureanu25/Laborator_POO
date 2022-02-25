46978681f2dc50a72ac3c7c032552b4b3a1dd6e7
StefanMagureanu25 <stefanmagureanu25@gmail.com>
2022-02-25 16:43:49 +0200

    tema 0 finished

--- tema0.cpp
+++ tema0.cpp
@@ -16,50 +16,76 @@ public:
         c1.b = b;
     }
 
-    void setA(double a_) {
+    void parte_reala(double a_) {
         a = a_;
     }
 
-    void setB(double b) {
-        Numar_Complex::b = b;
+    void parte_imaginara(double b_) {
+        b = b_;
     }
 
-    friend ostream &operator<<(ostream &os, const Numar_Complex &complex) {
+    friend ostream &operator<<(ostream &afis, const Numar_Complex &complex) {
         if (complex.b == 0)
-            os << complex.a;
+            afis << complex.a;
         else if (complex.a == 0)
-            os << complex.b << "i";
+            afis << complex.b << "i";
         else if (complex.b == 1)
-            os << complex.a << "+i";
+            afis << complex.a << "+i";
         else if (complex.b == -1)
-            os << complex.a << "-i";
+            afis << complex.a << "-i";
         else if (complex.b > 0)
-            os << complex.a << "+" << complex.b << "i";
+            afis << complex.a << "+" << complex.b << "i";
         else if (complex.b < 0)
-            os << complex.a << "-" << complex.b << "i";
-        return os;
+            afis << complex.a << complex.b << "i";
+        return afis;
     }
 
-    Numar_Complex operator+(const Numar_Complex &rhs) const {
-        return Numar_Complex(a + rhs.a,
-                             b + rhs.b);
+    friend Numar_Complex operator+(const Numar_Complex &c1, const Numar_Complex &c2) {
+        Numar_Complex S(0, 0);
+        S.a = c1.a + c2.a;
+        S.b = c1.b + c2.b;
+        return S;
+
     }
 
-    Numar_Complex operator*(const Numar_Complex &rhs) const {
-        return Numar_Complex(a * rhs.a,
-                             b * rhs.b);
+
+    friend Numar_Complex operator*(const Numar_Complex &c1, const Numar_Complex &c2) {
+        Numar_Complex P(0, 0);
+        P.a = c1.a * c2.a - c1.b * c2.b;
+        P.b = c1.a * c2.b + c2.a * c1.b;
+        return P;
     }
 
-    Numar_Complex operator/(const Numar_Complex &rhs) const {
-        return Numar_Complex(a / rhs.a,
-                             b / rhs.b);
+    friend Numar_Complex operator/(const Numar_Complex &c1, const Numar_Complex &c2) {
+        Numar_Complex I(0, 0);
+        I.a = (c1.a * c2.a + c1.b * c2.b) / (pow(c2.a, 2) + pow(c2.b, 2));
+        I.b = (c2.a * c1.b - c1.a * c2.b) / (pow(c2.a, 2) + pow(c2.b, 2));
+        return I;
     }
 
-    double modul(Numar_Complex c) {
-        return sqrt(pow(c.a, 2) + pow(c.b, 2));
+    double modul() {
+        return sqrt(pow(a, 2) + pow(b, 2));
     }
 };
 
 int main() {
-
+    double re, im;
+    cout << "Primul numar complex\n";
+    cout << "Introduceti partea reala:";
+    cin >> re;
+    cout << "Introduceti partea imaginara:";
+    cin >> im;
+    Numar_Complex a(re, im);
+    cout << "Al doilea numar complex\n";
+    cout << "Introduceti partea reala:";
+    cin >> re;
+    cout << "Introduceti partea imaginara:";
+    cin >> im;
+    Numar_Complex b(re, im);
+    cout << "Modulul primului numar complex este: " << a.modul() << "\n";
+    cout << "Modulul celui de-al doilea numar complex este: " << b.modul() << "\n";
+    cout << "Suma celor doua numere complexe este: " << a + b << "\n";
+    cout << "Produsul celor doua numere complexe este: " << a * b << "\n";
+    cout << "Raportul celor doua numere complexe este: " << a / b << "\n";
+    return 0;
 }
